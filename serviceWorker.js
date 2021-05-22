0feff92246f48cb1d551ffa86c724d0f9c502ba8
CrisSoria <crissoria07@gmail.com>
2021-05-22 17:35:25 -0300

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,43 @@
+//storage del browser que se mostrará cuando no exista conexión a internet
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html", "offline.html"];
+
+const self = this;
+
+// Install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html")); //si no se puede hacer el pedido, es porque está offline, entonces buscar en cache
+    })
+  );
+});
+
+// Activate the SW
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName); //borro las demás cache para así quedarme solo con la que necesito
+          }
+        })
+      )
+    )
+  );
+});
